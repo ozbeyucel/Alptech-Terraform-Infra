6d99855d9647168d791cec5b3803c3b95dd97dd5
Kadir Ozbey <ozbeyucel@gmail.com>
2025-05-30 11:43:42 -0400

    maintfupdated

--- main.tf
+++ main.tf
@@ -1,19 +1,46 @@
 terraform {
-  required_version = ">= 1.4.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = "~> 4.0"
     }
   }
+  required_version = ">= 1.0.0"
 
   backend "gcs" {
-    bucket  = "alptech-terraform-state"  # replace this after we create the bucket
-    prefix  = "state"
+    bucket  = "alptech-terraform-state"
+    prefix  = "gke"
   }
 }
 
 provider "google" {
   project = var.project_id
   region  = var.region
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.gke_cluster_name
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  networking_mode = "VPC_NATIVE"
+
+  ip_allocation_policy {}
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+
+  node_config {
+    machine_type = var.gke_node_machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
+  initial_node_count = var.gke_node_count
 }
